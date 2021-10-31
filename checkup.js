ee7b7d96770b08e04bddefbf058701c2e5390277
rubybb <10256477+rubybb@users.noreply.github.com>
2021-10-30 20:16:49 -0400

    feat: initial testing

---
+++ checkup.js
@@ -0,0 +1,26 @@
+import fs from "fs/promises";
+
+/* import { SystemApi } from "vrchat"; */
+import fetch from "node-fetch";
+import beautify from "js-beautify";
+
+const VRCHAT_API_URL = "https://vrchat.com/api/1"
+
+const fetchJs = () => fetch(`${VRCHAT_API_URL}/js/app.js`).then(r => r.text());
+const fetchCss = () => fetch(`${VRCHAT_API_URL}/css/app.css`).then(r => r.text());
+
+(async () => {
+    /* Fetch and beautify the current javascript bundle. */
+    const jsRaw = await fetchJs();
+    await fs.writeFile("./dist/raw/app.js", jsRaw);
+
+    const jsPretty = beautify.js(jsRaw, {});
+    await fs.writeFile("./dist/app.js", jsPretty);
+
+    /* Fetch and beautify the current css bundle. */
+    const cssRaw = await fetchCss();
+    await fs.writeFile("./dist/raw/app.css", cssRaw);
+
+    const cssPretty = beautify.css(cssRaw, {});
+    await fs.writeFile("./dist/app.css", cssPretty);
+})()
