5f7a734bd12e51a710e155858c0a179a64918aac
Aries <10256477+ariesclark@users.noreply.github.com>
2024-11-05 00:21:52 -0700

    fix: support vrchat's dynamic chunking

---
+++ eslint.config.mjs
@@ -0,0 +1,3 @@
+import { configure } from "@ariesclark/eslint-config"
+
+export default configure({})
--- scripts/checkup.js
+++ scripts/checkup.js
@@ -1,54 +1,133 @@
-import fs from "fs/promises";
+import fs from "node:fs/promises";
 
-import { SystemApi } from "vrchat";
+import { parse } from "@babel/parser";
+import traverse from "@babel/traverse";
 import beautify from "js-beautify";
+import { SystemApi } from "vrchat";
 
 const system = new SystemApi({
-    basePath: "https://vrchat.com/api/1",
-    baseOptions: {
-        headers: {
-            "User-Agent": "vrchat-monitor/1 vrchatapi.lpv0t@aries.fyi"
-        }
-    }
+	basePath: "https://vrchat.com/api/1",
+	baseOptions: {
+		headers: {
+			"User-Agent": "vrchat-monitor/1 vrchatapi.lpv0t@aries.fyi"
+		}
+	}
 });
 
-function findIntersection(arr) {
-    if (arr.length === 0) {
-      return {};
-    }
-    let intersection = {...arr[0]};
-    for (let i = 1; i < arr.length; i++) {
-      for (let key in intersection) {
-        if (!(key in arr[i])) {
-          delete intersection[key];
-        }
-      }
-    }
-    return intersection;
-  }
+function trim(value) {
+	if (!value || typeof value !== "string")
+		return value;
 
+	return value.replaceAll(/(^\s+|\s+$)/g, "");
+}
+
+function parseChunk(source) {
+	const identifiers = new Set();
+	const literals = new Set();
+	const probableChunks = new Map();
+
+	try {
+		const ast = parse(source, {});
+		traverse.default(ast, {
+			enter(path) {
+				if (path.isIdentifier())
+					identifiers.add(trim(path.node.name));
+
+				if (path.isLiteral())
+					literals.add(trim(path.node.value));
+
+				if (path.isObjectProperty()
+					&& path.node.key.type === "NumericLiteral"
+					&& path.node.value.type === "StringLiteral"
+					&& path.node.value.value.length === 64)
+					probableChunks.set(path.node.key.value, path.node.value.value);
+			},
+		});
+	}
+
+	catch (reason) {
+		console.warn("failed to parse chunk", reason);
+	}
+
+	const pretty = beautify.js(source, { unescape_strings: true });
+	return { source, pretty, identifiers, literals, probableChunks };
+}
+
+const protocols = ["http:", "https:", "ws:", "wss:", "vrchat:"];
 
 (async () => {
-    // Fetch and beautify the JavaScript bundle
-    const { data: jsRaw } = await system.getJavaScript("public", "main")
-    await fs.writeFile("./dist/raw/app.js", jsRaw);
+	await fs.rm("./dist/chunks", { recursive: true }).catch(() => { });
+	await fs.mkdir("./dist/chunks");
 
-    const jsPretty = beautify.js(jsRaw, { unescape_strings: true });
-    await fs.writeFile("./dist/app.js", jsPretty);
+	await fs.rm("./dist/raw/chunks", { recursive: true }).catch(() => { });
+	await fs.mkdir("./dist/raw/chunks");
 
-    const uniqueStrings = [...new Set([...jsPretty.matchAll(/(['"`])(.*?)(?<!\\)\1/gi)].map((match) => match[2].trim()))].sort()
-    await fs.writeFile("./dist/app.strings.json", JSON.stringify(uniqueStrings, null, 4));
+	// Fetch and beautify the JavaScript bundle
+	const { data: jsRaw } = await system.getJavaScript("public", "main");
+	await fs.writeFile("./dist/raw/app.js", jsRaw);
 
-    const uniqueKeys = [...new Set([...jsPretty.matchAll(/\b\w+\b(?!\s*\()/gi)].map((match) => match[0].trim()))].sort()
-    await fs.writeFile("./dist/app.keys.json", JSON.stringify(uniqueKeys, null, 4));
+	const { identifiers: _identifiers, literals: _literals, probableChunks, pretty } = parseChunk(jsRaw);
+	await fs.writeFile("./dist/app.js", pretty);
 
-    const uniqueUrls = [...new Set([...jsPretty.matchAll(/\b((?:\w+:)?\/\/(?:[\w-]+\.)+[a-z]{2,}(?:[\w\/+=%&_\.~?\-]*)\b)/gi)].map((match) => match[0].trim()))].sort()
-    await fs.writeFile("./dist/app.urls.json", JSON.stringify(uniqueUrls, null, 4));
+	console.log(probableChunks);
 
-    // Fetch and beautify the CSS bundle
-    const { data: cssRaw } = await system.getCSS()
-    await fs.writeFile("./dist/raw/app.css", cssRaw);
+	const chunks = (await Promise.all([...probableChunks.entries()].map(async ([id, hash]) => {
+		try {
+			const response = await fetch(`https://dtuitjyhwcl5y.cloudfront.net/${hash}.js`);
+			const source = await response.text();
 
-    const cssPretty = beautify.css(cssRaw, {});
-    await fs.writeFile("./dist/app.css", cssPretty);
-})()
+			const chunk = parseChunk(source);
+			await fs.writeFile(`./dist/raw/chunks/${id}.js`, source);
+			await fs.writeFile(`./dist/chunks/${id}.js`, chunk.pretty);
+
+			return chunk;
+		}
+		catch (reason) {
+			console.warn("failed to get chunk", id, reason);
+			return null;
+		}
+	}))).filter(Boolean);
+
+	let { identifiers, literals } = chunks.reduce((accumulator, chunk) => {
+		chunk.identifiers.forEach((value) => accumulator.identifiers.add(value));
+		chunk.literals.forEach((value) => accumulator.literals.add(value));
+		return accumulator;
+	}, { identifiers: _identifiers, literals: _literals });
+
+	identifiers = [..._identifiers].sort();
+	literals = [..._literals].sort();
+
+	const urls = literals.map((literal) => {
+		if (typeof literal !== "string") return null;
+
+		try {
+			const url = new URL(literal);
+			if (protocols.includes(url.protocol))
+				return url.href;
+
+			return null;
+		}
+		// eslint-disable-next-line unused-imports/no-unused-vars
+		catch (reason) {
+			const matches = [...literal.matchAll(/\bhttps?:\/\/[^\s'"]+/gi)]
+				.map((match) => trim(match[0]))
+				.filter(Boolean);
+
+			if (matches.length > 0)
+				return matches;
+
+			return null;
+		}
+	}).flat().filter(Boolean);
+
+	await fs.writeFile(`./dist/identifiers.json`, JSON.stringify(identifiers, null, 2));
+	await fs.writeFile(`./dist/literals.json`, JSON.stringify(literals.sort(), null, 2));
+	await fs.writeFile(`./dist/urls.json`, JSON.stringify(urls.sort(), null, 2));
+
+	// Fetch and beautify the CSS bundle
+	const { data: cssRaw } = await system.getCSS();
+	await fs.writeFile("./dist/raw/app.css", cssRaw);
+
+	const cssPretty = beautify.css(cssRaw, {});
+	await fs.writeFile("./dist/app.css", cssPretty);
+})();
